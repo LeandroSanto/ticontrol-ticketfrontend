6f327d637e1cf899b6c193c695ec44eb2cdaf7b5
Leandro Santo <leandro.santo@outlook.com>
2022-01-18 17:42:20 -0300

    correção da activity

--- src/pages/OrderService/index.tsx
+++ src/pages/OrderService/index.tsx
@@ -5,10 +5,22 @@ import { Button } from '../../components/Button';
 
 import { Header } from '../../components/Header';
 import { OrderBar } from '../../components/OrderBar';
+import { Activity } from '../../components/Activity';
 
 import './styles.css';
 
+let activityDemo: {
+  user: string,
+  activity: string
+}
+
+
+
+
 export function OrderService(){
+  
+  activityDemo.user='Leandro';
+  activityDemo.activity = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru';
 
 return(
   <div>
@@ -64,8 +76,14 @@ return(
 
       <div className='orderContent'>
         <OrderBar />
+        
+        <div>
+          <Activity activity={activityDemo}/>
+        </div>
+
       </div>
 
+   
   </div>
 )
 }
